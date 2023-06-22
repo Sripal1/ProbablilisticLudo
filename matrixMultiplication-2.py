16752e8f6a490d28f93521e0ae0b4e44d637b799
spalaniappan9 <spalaniappan9@gatech.edu>
2023-06-22 11:36:03 +0530

    Logic for Pd

--- matrixMultiplication-2.py
+++ matrixMultiplication-2.py
@@ -33,20 +33,54 @@ for line in modified_array:
     # print("\n\n")
     finalArr.append(line)
 
-print(finalArr)
+# print(finalArr)
 print(f"Number of rows {len(finalArr)}")
 print(f"Number of columns {len(finalArr[0])}")
 
 finalArr = np.array(finalArr)
 multiplier = np.array(finalArr)
 result = finalArr * multiplier
-print(result)
+# print(result)
 
 df = pd.DataFrame(result)
 output_file = 'output.xlsx'
 df.to_excel(output_file, index=False)
-# subprocess.call(['open', outputA_file])
+# subprocess.call(['open', output_file])
+# # Open the Excel file
+# excel_file = openpyxl.load_workbook(output_file)
 
-subprocess.call(['open', output_file])
-# Open the Excel file
-excel_file = openpyxl.load_workbook(output_file) 
+
+'''CALCULATING Pd'''
+
+def getotherColorPositionVector(positionIdx):
+    column = [row[positionIdx+4] for row in finalArr]
+    print(f"Other color position vector : {column[:len(column)-4]}")
+    return column[:len(column)-4]
+
+def getGreenPosVector(positionIdx):
+    vector = []
+    for i in range(81):
+        if i == positionIdx+4:
+            vector.append(1)
+        else:
+            vector.append(0)
+    print(f"Green positition vector : {vector}")
+    return vector
+
+currGreenPos = 4; # NULL spot
+PgreenCaptured = None
+
+def findGreenCoinPos(greenPosVec):
+    for i in range(len(greenPosVec)):
+        if greenPosVec[i] == 1:
+            currGreenPos = i
+            print(f"Current green position : {currGreenPos}")
+            return currGreenPos
+
+def checkIntersection(greenPosVec,otherClrProbVec):
+    currentGreenPosition = findGreenCoinPos(greenPosVec)
+    if otherClrProbVec[currentGreenPosition+1] != 0:    #SHOULD I ADD 1?
+        PgreenCaptured = otherClrProbVec[currentGreenPosition+1]
+        print(f"Likelihood of green captured: {PgreenCaptured*100}%")
+
+checkIntersection(getGreenPosVector(7),getotherColorPositionVector(7))
